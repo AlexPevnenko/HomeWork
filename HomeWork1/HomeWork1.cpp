45f53ea2f7addb7a9cb9c1a6bfb321f0cef05e18
Alex <alex.pevnenko@gmail.com>
2021-09-09 21:32:32 +0300

    игнорю сишный файл

--- HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1.cpp
@@ -3,77 +3,81 @@
 #include "math.h"
 #include "assert.h"
 
-#define SS_INF_ROOTS 5
-#define SS_IM_ROOTS 6
-#define SS_ER_A 7
-
 bool check_number(int,int,int);
 void find_roots(double, double, double);
-
-
 
 int main() 
 {
     setlocale(LC_ALL,"Russian");
+
     double a, b, c;
     int test_a, test_b, test_c;
-    //ввод работает четко
+
     printf("Введите коэффициенты квадратного уравнения:\n");
-    test_a=scanf_s("%lg", &a);
-    test_b=scanf_s("%lg", &b);
-    test_c=scanf_s("%lg", &c);
-    //все супер , классно , но нет проверки , что введено - число или говно 
+
+    test_a = scanf_s ("%lg", &a);
+
+    test_b = scanf_s ("%lg", &b);
+
+    test_c = scanf_s ("%lg", &c);
+
     bool test = check_number(test_a, test_b, test_c);
+
     if (test == false) 
     {
         printf("\nВведенные значения не являются корректными!\n");
         return 1;
     }
+
     else
     {
-        //теперь находим сколько корней должно быть 
-        find_roots(a, b, c);
+        find_roots (a, b, c);
         return 0;
     }
 }
 
-void find_roots(double a, double b, double c) 
+void find_roots (double a, double b, double c) 
 {
-    if (a != 0 && b == 0 && c == 0) //v
+    if (a != 0 && b == 0 && c == 0) 
     {
         printf("Корень равен 0");
     }
+
     else if (a != 0)
     {
         double D = b * b - 4 * a * c;
-        if (D > 0) //v
+        if (D > 0) 
         {
             double x_1 = (-b + sqrt(D)) / (2 * a);
             double x_2 = (-b - sqrt(D)) / (2 * a);
             printf("\nКорни квадратного уравнения равны:\nx_1=%lg\nx_2=%lg\n", x_1, x_2);
         }
-        else if (D == 0) //v
+
+        else if (D == 0) 
         {
             double x = (-b) / (2 * a);
             printf("\nКорень квадратного уравнения равен:\nx=%lg\n", x);
         }
-        else //v
+
+        else 
         {
             printf("\nДействительных корней данное уравнение не имеет!\n");
         }
 
     }
-    else //v
+
+    else 
     {
         printf("Корень- любое число\n");
     }
 }
 
-bool check_number(int test_a, int test_b, int test_c) 
+bool check_number (int test_a, int test_b, int test_c) 
 {
     if (test_a == 0 || test_b == 0 || test_c == 0)
     {
         return false;
     }
+
     else return true;
 }
